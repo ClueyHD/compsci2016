8b3697e94fb1158a4f824880f3c2460d328643de
ClueyHD <steven.perscacros@gmail.com>
2016-10-13 20:12:01 +1100

    data extraction sample source code

---
+++ data_Extraction.py
@@ -0,0 +1,178 @@
+from lxml import etree
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+from matplotlib.collections import PatchCollection
+from mpl_toolkits.basemap import Basemap
+from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
+from shapely.prepared import prep
+from pysal.esda.mapclassify import Natural_Breaks as nb
+from descartes import PolygonPatch
+import fiona
+from itertools import chain
+
+#Now, we’re going to extract all the useful XML data into a dict.
+tree = etree.parse("data/london_20131229.xml")
+root = tree.getroot()
+
+output = dict()
+output['raw'] = []
+output['crs'] = []
+output['lon'] = []
+output['lat'] = []
+
+for each in root.xpath('/openplaques/plaque/geo'):
+    # check what we got back
+    output['crs'].append(each.get('reference_system'))
+    output['lon'].append(each.get('longitude'))
+    output['lat'].append(each.get('latitude'))
+    # now go back up to plaque
+    r = each.getparent().xpath('inscription/raw')[0]
+    if isinstance(r.text, str):
+        output['raw'].append(r.text.lstrip().rstrip())
+    else:
+        output['raw'].append(None)
+
+#This will produce a dict containing the coordinate reference system, longitude, latitude, and description of each plaque record. Next, we’re going to create a Pandas DataFrame, drop all records which don’t contain a description, and convert the long and lat values from string to floating-point numbers.
+df = pd.DataFrame(output)
+df = df.replace({'raw': 0}, None)
+df = df.dropna()
+df[['lon', 'lat']] = df[['lon', 'lat']].astype(float)
+
+#Now, we’re going to open our shapefile, and get some data out of it, in order to set up our basemap:
+shp = fiona.open('data/london_wards.shp')
+bds = shp.bounds
+shp.close()
+extra = 0.01
+ll = (bds[0], bds[1])
+ur = (bds[2], bds[3])
+coords = list(chain(ll, ur))
+w, h = coords[2] - coords[0], coords[3] - coords[1]
+
+#We’ve done two things here:
+#1.extracted the map boundaries
+#2.Calculated the extent, width and height of our basemap
+#We’re ready to create a Basemap instance, which we can use to plot our maps on
+
+m = Basemap(
+    projection='tmerc',
+    lon_0=-2.,
+    lat_0=49.,
+    ellps = 'WGS84',
+    llcrnrlon=coords[0] - extra * w,
+    llcrnrlat=coords[1] - extra + 0.01 * h,
+    urcrnrlon=coords[2] + extra * w,
+    urcrnrlat=coords[3] + extra + 0.01 * h,
+    lat_ts=0,
+    resolution='i',
+    suppress_ticks=True)
+m.readshapefile(
+    'data/london_wards',
+    'london',
+    color='none',
+    zorder=2)
+
+#I’ve chosen the transverse mercator projection, because it exhibits less distortion over areas with a small east-west extent. This projection requires us to specify a central longitude and latitude, which I’ve set as -2, 49.
+# set up a map dataframe
+df_map = pd.DataFrame({
+    'poly': [Polygon(xy) for xy in m.london],
+    'ward_name': [ward['NAME'] for ward in m.london_info]})
+df_map['area_m'] = df_map['poly'].map(lambda x: x.area)
+df_map['area_km'] = df_map['area_m'] / 100000
+
+# Create Point objects in map coordinates from dataframe lon and lat values
+map_points = pd.Series(
+    [Point(m(mapped_x, mapped_y)) for mapped_x, mapped_y in zip(df['lon'], df['lat'])])
+plaque_points = MultiPoint(list(map_points.values))
+wards_polygon = prep(MultiPolygon(list(df_map['poly'].values)))
+# calculate points that fall within the London boundary
+ldn_points = filter(wards_polygon.contains, plaque_points)
+
+# Convenience functions for working with colour ramps and bars
+def colorbar_index(ncolors, cmap, labels=None, **kwargs):
+    """
+    This is a convenience function to stop you making off-by-one errors
+    Takes a standard colour ramp, and discretizes it,
+    then draws a colour bar with correctly aligned labels
+    """
+    cmap = cmap_discretize(cmap, ncolors)
+    mappable = cm.ScalarMappable(cmap=cmap)
+    mappable.set_array([])
+    mappable.set_clim(-0.5, ncolors+0.5)
+    colorbar = plt.colorbar(mappable, **kwargs)
+    colorbar.set_ticks(np.linspace(0, ncolors, ncolors))
+    colorbar.set_ticklabels(range(ncolors))
+    if labels:
+        colorbar.set_ticklabels(labels)
+    return colorbar
+
+def cmap_discretize(cmap, N):
+    """
+    Return a discrete colormap from the continuous colormap cmap.
+
+        cmap: colormap instance, eg. cm.jet.
+        N: number of colors.
+
+    Example
+        x = resize(arange(100), (5,100))
+        djet = cmap_discretize(cm.jet, 5)
+        imshow(x, cmap=djet)
+
+    """
+    if type(cmap) == str:
+        cmap = get_cmap(cmap)
+    colors_i = np.concatenate((np.linspace(0, 1., N), (0., 0., 0., 0.)))
+    colors_rgba = cmap(colors_i)
+    indices = np.linspace(0, 1., N + 1)
+    cdict = {}
+    for ki, key in enumerate(('red', 'green', 'blue')):
+        cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki]) for i in xrange(N + 1)]
+    return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
+
+# draw ward patches from polygons
+df_map['patches'] = df_map['poly'].map(lambda x: PolygonPatch(
+    x,
+    fc='#555555',
+    ec='#787878', lw=.25, alpha=.9,
+    zorder=4))
+
+plt.clf()
+fig = plt.figure()
+ax = fig.add_subplot(111, axisbg='w', frame_on=False)
+
+# we don't need to pass points to m() because we calculated using map_points and shapefile polygons
+dev = m.scatter(
+    [geom.x for geom in ldn_points],
+    [geom.y for geom in ldn_points],
+    5, marker='o', lw=.25,
+    facecolor='#33ccff', edgecolor='w',
+    alpha=0.9, antialiased=True,
+    label='Blue Plaque Locations', zorder=3)
+# plot boroughs by adding the PatchCollection to the axes instance
+ax.add_collection(PatchCollection(df_map['patches'].values, match_original=True))
+# copyright and source data info
+smallprint = ax.text(
+    1.03, 0,
+    'Total points: %s\nContains Ordnance Survey data\n$\copyright$ Crown copyright and database right 2013\nPlaque data from http://openplaques.org' % len(ldn_points),
+    ha='right', va='bottom',
+    size=4,
+    color='#555555',
+    transform=ax.transAxes)
+
+# Draw a map scale
+m.drawmapscale(
+    coords[0] + 0.08, coords[1] + 0.015,
+    coords[0], coords[1],
+    10.,
+    barstyle='fancy', labelstyle='simple',
+    fillcolor1='w', fillcolor2='#555555',
+    fontcolor='#555555',
+    zorder=5)
+plt.title("Blue Plaque Locations, London")
+plt.tight_layout()
+# this will set the image width to 722px at 100dpi
+fig.set_size_inches(7.22, 5.25)
+plt.savefig('data/london_plaques.png', dpi=100, alpha=True)
+plt.show()
