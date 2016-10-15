c5252e2e934ea077dd40ce78d4dd3d24105c0ec2
MceeL <cho0019@jmss.vic.edu.au>
2016-10-15 15:20:15 +1100

    actually functioning code

--- 15.10.16-Ecocampus-Graphs.py
+++ 15.10.16-Ecocampus-Graphs.py
@@ -3,8 +3,12 @@
 
 import csv
 
+
 names = []
 count = 0
+
+name = []
+counts = []
 
 file_open = "data.csv"
 
@@ -20,4 +24,12 @@ for i in data:
         names.append(i[11])
 
 for c in names:
-    print(c)
+    if c in name:
+        #count.append(name.index(c))
+        print(name.index(c))
+        counts.insert(name.index(c), counts[name.index(c)]+1)
+    else:
+        name.append(c)
+        counts.append(1)
+print(name)
+print(counts)
